b25f70dc7fbbf0fe1db0a122ad9afad3c68274e6
石季凡 <136095349+SJF-ECNU@users.noreply.github.com>
2023-09-20 16:09:27 +0800

    修改了lesson2中question1的错误算法

--- lesson2/question1.py
+++ lesson2/question1.py
@@ -1,42 +1,34 @@
-# 拆分函数，用于将一个数字二分
-def divide(input_num):
-    if input_num % 2 == 0:
-        return int(input_num / 2)
-    else:
-        return int((input_num + 1) / 2)
-
-
-# 不断调用拆分函数直到拆分为2和3
 def max_list(input_num):
-    my_dict = {}
-    my_dict[input_num] = my_dict.get(input_num, 0) + 1  # 通过字典记录输入的数值
-
-    while True:  # 通过循环来实现而不是递归，递归的时间开销更大
-        max_key = max(my_dict.keys())  # 找到字典中最大的键值
-        if max_key <= 3:  # 检测这个键值是否大于3，如果没有大于3的，说明已经分解完成
+    num_undivide = input_num
+    count_2 = 0
+    count_3 = 0
+    while True:
+        if num_undivide == 4:
+            count_2 += 2
             break
+        if num_undivide == 2:
+            count_2 += 1
+            break
+        if num_undivide == 3:
+            count_3 += 1
+            break
+        count_3 += 1
+        num_undivide -= 3
+    return {2: count_2, 3: count_3}
 
-        new_input = divide(max_key)
-        my_dict[new_input] = my_dict.get(new_input, 0) + 1
-        my_dict[max_key - new_input] = my_dict.get(max_key - new_input, 0) + 1
-        my_dict[max_key] -= 1
 
-        if my_dict[max_key] == 0:
-            del my_dict[max_key]
-    return my_dict
-
-
-num_to_divide = int(input("请输入要拆分的数字："))
-return_dict = max_list(num_to_divide)
-
-# 输出函数
-while True:
-    max_key = max(return_dict.keys())
-    print(max_key, end='')
-    return_dict[max_key] -= 1
-    if return_dict[max_key] == 0:
-        del return_dict[max_key]
-    if not return_dict:
-        break
-    else:
-        print("*", end='')
+if __name__ == "__main__":
+    num_to_divide = int(input("请输入要拆分的数字："))
+    my_dict=max_list(num_to_divide)
+    for i in range(0,my_dict[3]):
+        print(3,end=" ")
+        if(i!=my_dict[3]-1):
+            print("*",end="")
+    if my_dict[2]!=0:
+        print("*",end="")
+    for i in range(0,my_dict[2]):
+        print(2,end="")
+        if(i!=my_dict[2]-1):
+            print("*",end="")
+        
+        
